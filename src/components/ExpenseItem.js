87aa65746c50dea0f0721532b305f463dc5ee3c5
Andrei <andrei@nerdai.io>
2022-04-02 16:10:32 -0400

    add a wrapper component Card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
@@ -6,13 +7,13 @@ function ExpenseItem(props) {
   // the component. Note: only have one root element in each return JSX snippet
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}/> 
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>$ {props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
