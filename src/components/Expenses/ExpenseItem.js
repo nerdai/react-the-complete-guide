0b8e076ba1cd18c97567b6d8605c3c46652c2596
Andrei <andrei@nerdai.io>
2022-04-09 23:42:59 -0400

    added state for title; watched up to video 51

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   // props is an object containing the properties or attributes associate with
   // the component. Note: only have one root element in each return JSX snippet
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/> 
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>$ {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
