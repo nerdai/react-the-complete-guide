816e409c004432b4a0368dad609c10e987c88d6e
Andrei <andrei@nerdai.io>
2022-07-20 11:31:56 -0400

    lift userlist state up to app

--- section8/src/components/Users/AddUser.js
+++ section8/src/components/Users/AddUser.js
@@ -17,6 +17,7 @@ const AddUser = props => {
     if (+enteredAge < 1) {
       return;
     }
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredAge('');
     setEnteredUsername('');
   };
