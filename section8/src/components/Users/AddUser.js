23fe4593f3c1afafdd73602c6edf1e8f5a4a083d
Andrei <andrei@nerdai.io>
2022-07-20 10:56:08 -0400

    add managing of User states

--- section8/src/components/Users/AddUser.js
+++ section8/src/components/Users/AddUser.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    setEnteredAge('');
+    setEnteredUsername('');
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return(
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} value={enteredUsername} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} value={enteredAge} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
