14e7438b3e88f1901e1d869eba6ed3f98b279f9a
Andrei <andrei@nerdai.io>
2022-08-07 23:16:20 -0400

    implement the Cart Modal

--- section11/src/components/Cart/Cart.js
+++ section11/src/components/Cart/Cart.js
@@ -1,20 +1,21 @@
 import React from "react";
 
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
-      <div>
+      <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
@@ -22,7 +23,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
