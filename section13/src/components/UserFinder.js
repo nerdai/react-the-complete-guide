129d4332e6dae0f8f44efe5d08f30971dd91d69f
Andrei <andrei@nerdai.io>
2022-09-28 22:06:54 -0400

    use statewide state via context for class-based components

--- section13/src/components/UserFinder.js
+++ section13/src/components/UserFinder.js
@@ -1,14 +1,11 @@
 import { Fragment, Component } from "react";
+import UsersContext from "../store/users-context";
 
 import Users from "./Users";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
-
 class UserFinder extends Component {
+  static contextType = UsersContext; // can only set static contextType property once
+
   constructor() {
     super();
     this.state = {
@@ -21,15 +18,16 @@ class UserFinder extends Component {
     this.setState({ searchTerm: event.target.value });
   }
 
-  componentDidMount() { // only run the first time after it is rendered
+  componentDidMount() {
+    // only run the first time after it is rendered
     // Send http request ..
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
