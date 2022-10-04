4ae0653f1948837002fab3cfeeb42466dc92de5b
Andrei <andrei@nerdai.io>
2022-10-04 15:57:46 -0400

    prepare project for post request

--- section14-old/src/App.js
+++ section14-old/src/App.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback(async () => {
+  const fetchMovitesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
-
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -36,12 +34,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
-
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+    fetchMovitesHandler();
+  }, [fetchMovitesHandler]);
 
   let content = <p>Found no movies.</p>;
 
@@ -60,10 +54,7 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={fetchMovitesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
     </React.Fragment>
