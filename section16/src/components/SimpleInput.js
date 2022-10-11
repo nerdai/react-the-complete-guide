3d33d94258bdf6ac9f970b5a538f8b3c09a09e8c
Andrei <andrei@nerdai.io>
2022-10-11 12:14:46 -0400

    up to video 204

--- section16/src/components/SimpleInput.js
+++ section16/src/components/SimpleInput.js
@@ -1,14 +1,23 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+    if (event.target.value.trim() !== "") {
+      setEnteredNameIsValid(true);
+    }
   };
+
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+    } 
+  }
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
@@ -24,9 +33,6 @@ const SimpleInput = (props) => {
 
     console.log(enteredName);
 
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
-
     setEnteredName("");
   };
 
@@ -41,10 +47,10 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
         {nameInputIsValid && (
