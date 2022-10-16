9aa2c36a2e7342adb53aeef3032ceed73be0dda5
Andrei <andrei@nerdai.io>
2022-10-15 21:35:45 -0400

    add dispatch of actions in basic redux intro

--- section18/redux-basics/redux-demo.js
+++ section18/redux-basics/redux-demo.js
@@ -1,9 +1,20 @@
 import { legacy_createStore as createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1
-  };
+
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    };
+  }
+
+  return state;
 };
 
 const store = createStore(counterReducer);
@@ -17,3 +28,4 @@ store.subscribe(counterSubscriber);
 
 // dispatch an action
 store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
