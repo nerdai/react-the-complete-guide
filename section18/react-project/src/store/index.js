4e0c30c6092ea3594f22c6f6777995fa802774b6
Andrei <andrei@nerdai.io>
2022-10-18 23:17:43 -0400

    add a new slice for auth

--- section18/react-project/src/store/index.js
+++ section18/react-project/src/store/index.js
@@ -1,7 +1,5 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
-
 const counterSlice = createSlice({
   name: "counter",
   initialState: { counter: 0, showCounter: true },
@@ -23,10 +21,27 @@ const counterSlice = createSlice({
   },
 });
 
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: { isAuthenticated: false },
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    }
+  }
+})
+
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer
+  }
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
