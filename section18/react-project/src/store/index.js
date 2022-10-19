69cddd40455e65c6aeb92fefe60121401d24840a
Andrei <andrei@nerdai.io>
2022-10-18 22:56:09 -0400

    install redux toolkit, and create a state slice

--- section18/react-project/src/store/index.js
+++ section18/react-project/src/store/index.js
@@ -1,6 +1,29 @@
 import { legacy_createStore as createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState: { counter: 0, showCounter: true },
+  reducers: {
+    // allowed to "mutate" state here
+    // handled internally translated to immutable code
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
