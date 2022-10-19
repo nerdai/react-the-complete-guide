63f372cfefedceee9db01f79bc6f1edf5ddea968
Andrei <andrei@nerdai.io>
2022-10-18 23:47:44 -0400

    refactor code by splitting up index.js of store

--- section18/react-project/src/store/index.js
+++ section18/react-project/src/store/index.js
@@ -1,47 +1,13 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: { counter: 0, showCounter: true },
-  reducers: {
-    // allowed to "mutate" state here
-    // handled internally translated to immutable code
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: { isAuthenticated: false },
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    }
-  }
-})
+import counterReducer from "./counter";
+import authReducer from "./auth"
 
 const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
-    auth: authSlice.reducer
+    counter: counterReducer,
+    auth: authReducer
   }
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
