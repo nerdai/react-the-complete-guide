f00e5abdacc3cdc2b822749b06bd29e06acef825
Andrei <andrei@nerdai.io>
2022-10-19 23:13:01 -0400

    start wiring up cartSlice in components

--- section19/src/store/cart-slice.js
+++ section19/src/store/cart-slice.js
@@ -16,14 +16,27 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
+
+      state.totalQuantity++;
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+
+      state.totalQuantity--;
+    },
   },
 });
 
