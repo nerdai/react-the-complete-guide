5c30142b56f1de30cf5693126113b5681e9910ec
Andrei <andrei@nerdai.io>
2022-10-22 23:06:51 -0400

    Add MainHeader and Link component

--- section20/src/App.js
+++ section20/src/App.js
@@ -1,17 +1,23 @@
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
