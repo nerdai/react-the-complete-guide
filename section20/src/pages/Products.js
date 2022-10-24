c398202e708b41784d5af41047eb18480559589f
Andrei <andrei@nerdai.io>
2022-10-23 21:26:54 -0400

    using Switch and kw exact for configuring routes

--- section20/src/pages/Products.js
+++ section20/src/pages/Products.js
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Car</li>
-        <li>A Deck</li>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Car</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">A Deck</Link>
+        </li>
       </ul>
     </section>
-  )
-  
-
+  );
 };
 
 export default Products;
