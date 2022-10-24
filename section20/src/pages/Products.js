8847caaa7ef9a2b14e5146fd183a9f3148c6fa24
Andrei <andrei@nerdai.io>
2022-10-23 21:16:37 -0400

    create a dynamic route for products

--- section20/src/pages/Products.js
+++ section20/src/pages/Products.js
@@ -1,5 +1,16 @@
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Car</li>
+        <li>A Deck</li>
+      </ul>
+    </section>
+  )
+  
+
 };
 
 export default Products;
