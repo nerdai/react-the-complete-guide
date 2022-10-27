10a364d917ec594a60d84c3e0cff11528f1060c0
Andrei <andrei@nerdai.io>
2022-10-26 22:25:39 -0400

    using Route to conditionally render components based on the url

--- section20/practice-project/src/pages/QuoteDetail.js
+++ section20/practice-project/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { Link, useParams, Route } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -16,12 +16,19 @@ const QuoteDetail = () => {
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No Quote Found!</p>
+    return <p>No Quote Found!</p>;
   }
 
   return (
     <Fragment>
       <HighlightedQuote author={quote.author} text={quote.text} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
