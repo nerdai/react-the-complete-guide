a9284fb26ee69751c40a60a4fbb6d25d7bbe4eb4
Andrei <andrei@nerdai.io>
2022-10-27 22:10:06 -0400

    use a bit more convneient hooks for nested routes

--- section20/practice-project/src/pages/QuoteDetail.js
+++ section20/practice-project/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Link, useParams, Route } from "react-router-dom";
+import { Link, useParams, Route, useRouteMatch } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -11,6 +11,7 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
@@ -22,14 +23,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote author={quote.author} text={quote.text} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
