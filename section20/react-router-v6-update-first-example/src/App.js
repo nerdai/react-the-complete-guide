df30cbf653d7813f5c5ba9cdba45466dcd62900c
Andrei <andrei@nerdai.io>
2022-10-30 00:49:40 -0400

    complete first example

--- section20/react-router-v6-update-first-example/src/App.js
+++ section20/react-router-v6-update-first-example/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/welcome' element={<Welcome />} />
-          <Route exact path='/products' element={<Products />} />
+          <Route path='/products' element={<Products />} />
           <Route path='/products/:productId' element={<ProductDetail />} />
         </Routes>
       </main>
