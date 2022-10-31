c8c3aa6736b4b67cc78daab8d82cac78d773e7fa
Andrei <andrei@nerdai.io>
2022-10-30 23:48:18 -0400

    update how we do error handling with react router

--- section20/react-router-6.4-intro-react-router-6.3/src/App.jsx
+++ section20/react-router-6.4-intro-react-router-6.3/src/App.jsx
@@ -1,26 +1,39 @@
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-import BlogLayout from './pages/BlogLayout';
-import BlogPostsPage, { loader as blogPostsLoader } from './pages/BlogPosts';
-import NewPostPage from './pages/NewPost';
-import PostDetailPage, { loader as blogPostLoader } from './pages/PostDetail';
-import RootLayout from './pages/RootLayout';
-import WelcomePage from './pages/Welcome';
+import BlogLayout from "./pages/BlogLayout";
+import BlogPostsPage, { loader as blogPostsLoader } from "./pages/BlogPosts";
+import NewPostPage from "./pages/NewPost";
+import PostDetailPage, { loader as blogPostLoader } from "./pages/PostDetail";
+import RootLayout from "./pages/RootLayout";
+import WelcomePage from "./pages/Welcome";
+import ErrorPage from "./pages/Error";
 
-const router = createBrowserRouter(createRoutesFromElements(
-<Route path="/" element={<RootLayout />}>
-  <Route index element={<WelcomePage />} />
-  <Route path="/blog" element={<BlogLayout />}>
-    <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-    <Route path=":id" element={<PostDetailPage />} loader={blogPostLoader} />
-  </Route>
-  <Route path="/blog/new" element={<NewPostPage />} />
-</Route>));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={blogPostLoader}
+          errorElement={<p>An error occurred!</p>}
+        />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
