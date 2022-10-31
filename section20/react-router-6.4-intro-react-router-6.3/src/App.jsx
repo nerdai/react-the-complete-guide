66f92f910c0ccf93efc16c2554a3bf2f887ddaa2
Andrei <andrei@nerdai.io>
2022-10-30 23:42:21 -0400

    update postDetail component to react 6.4

--- section20/react-router-6.4-intro-react-router-6.3/src/App.jsx
+++ section20/react-router-6.4-intro-react-router-6.3/src/App.jsx
@@ -3,7 +3,7 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, R
 import BlogLayout from './pages/BlogLayout';
 import BlogPostsPage, { loader as blogPostsLoader } from './pages/BlogPosts';
 import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
+import PostDetailPage, { loader as blogPostLoader } from './pages/PostDetail';
 import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 
@@ -12,7 +12,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route index element={<WelcomePage />} />
   <Route path="/blog" element={<BlogLayout />}>
     <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-    <Route path=":id" element={<PostDetailPage />} />
+    <Route path=":id" element={<PostDetailPage />} loader={blogPostLoader} />
   </Route>
   <Route path="/blog/new" element={<NewPostPage />} />
 </Route>));
