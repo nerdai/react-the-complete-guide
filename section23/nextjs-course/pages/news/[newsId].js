5478a05162feb517184cc18580eedb0f26a7469d
Andrei <andrei@nerdai.io>
2022-11-01 22:10:41 -0400

    extract dynamic params from router

--- section23/nextjs-course/pages/news/[newsId].js
+++ section23/nextjs-course/pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 function DetailsPage() {
+  const router = useRouter();
+
+  console.log(router.query.newsId);
+
   return <h1>The News Details Page</h1>;
 }
 
