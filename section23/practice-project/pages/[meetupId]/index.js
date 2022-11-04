a867e0f112683a9a53b3c56da55fd5420aead270
Andrei <andrei@nerdai.io>
2022-11-03 22:50:18 -0400

    add MeetupDetail component

---
+++ section23/practice-project/pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      src="https://d3p2634vqbf9ci.cloudfront.net/8_iai%20pets_78.png"
+      alt="A Firs Meetup"
+      title="A First Meetup"
+      address="Some Stree 5, Some City"
+      description="The meetup description"
+    ></MeetupDetail>
+  );
+}
+
+export default MeetupDetails;
