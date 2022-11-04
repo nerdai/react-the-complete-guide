235de951e3f332f963482d15ca4558b80f2a6ebf
Andrei <andrei@nerdai.io>
2022-11-03 22:25:16 -0400

    update Link not defined

--- section23/practice-project/pages/index.js
+++ section23/practice-project/pages/index.js
@@ -1,24 +1,29 @@
 import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
 
 const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "A First Meetup",
     image: "https://d3p2634vqbf9ci.cloudfront.net/8_iai%20pets_78.png",
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
     id: "m2",
     title: "A First Meetup",
     image: "https://d3p2634vqbf9ci.cloudfront.net/8_iai%20pets_81.png",
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
 ];
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
 }
 
 export default HomePage;
