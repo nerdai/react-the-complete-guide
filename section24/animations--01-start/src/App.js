ec052a77d61b1c8e979ad57d1fd7a070ec27469a
Andrei <andrei@nerdai.io>
2022-12-01 22:29:04 -0500

    add react transition groups and go through first lesson

--- section24/animations--01-start/src/App.js
+++ section24/animations--01-start/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,11 +9,12 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
     return this.setState({ modalIsOpen: true });
-  }
+  };
 
   closeModal = () => {
     this.setState({ modalIsOpen: false });
@@ -22,9 +24,39 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        {this.state.modalIsOpen ? <Modal closed={this.closeModal}/> : null}
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 0.3s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+        {this.state.modalIsOpen ? <Modal closed={this.closeModal} /> : null}
         {this.state.modalIsOpen ? <Backdrop /> : null}
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
