666064a70318527d94eaa0e43cc5421dbf0f779a
Andrei <andrei@nerdai.io>
2023-01-04 22:46:05 -0500

    more interesting unit tests with rendering of button user events and state changes

--- section26/src/components/Greeting.test.js
+++ section26/src/components/Greeting.test.js
@@ -1,16 +1,42 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-describe('<Greeting />', () => {
-  test('renders Hello World as a text', () => {
+describe("<Greeting />", () => {
+  test("renders Hello World as a text", () => {
     // Arrange
     render(<Greeting />);
-  
+
     // Act
     /// ... nothing
-  
+
     // Assert
-    const helloWorldElement = screen.getByText('Hello World', { exact: false });
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    /// ... nothing
+
+    // Assert
+    const outputElement = screen.getByText("It's good to see you.");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders Changed! if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
   });
 });
