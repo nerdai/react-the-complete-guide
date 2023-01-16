78dadb6133799979d32bc5c34feb267defbc6b0a
Andrei <andrei@nerdai.io>
2023-01-15 23:49:25 -0500

    array and object types plus their combo

--- section27/basics.ts
+++ section27/basics.ts
@@ -13,3 +13,25 @@ userName = 'Max';
 
 let isInstructor: boolean;
 isInstructor = true;
+
+// more complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+// object type
+let person: {
+  name: string;
+  age: number
+};
+
+person = {
+  name: 'Max',
+  age: 32
+};
+
+let people: {
+  name: string;
+  age: number
+}[];
