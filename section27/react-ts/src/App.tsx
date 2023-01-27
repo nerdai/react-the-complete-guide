5da3d414b0600fb27ce344186c5ee35defd63974
Andrei <andrei@nerdai.io>
2023-01-26 22:38:34 -0500

    add some basic func with components and tsx

--- section27/react-ts/src/App.tsx
+++ section27/react-ts/src/App.tsx
@@ -1,7 +1,12 @@
+import Todos from "./components/Todos";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
