c0fb8563c564fa8a0bc3d87f3f6b61120efa0bb7
Andrei <andrei@nerdai.io>
2023-01-27 23:33:11 -0500

    added a Data model for our Todos object

--- section27/react-ts/src/App.tsx
+++ section27/react-ts/src/App.tsx
@@ -1,12 +1,16 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 import "./App.css";
 
-const DUMMY_ITEMS = ["Learn React", "Learn Typescript"];
-
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ]
+
   return (
     <div>
-      <Todos items={DUMMY_ITEMS} />
+      <Todos items={todos} />
     </div>
   );
 }
