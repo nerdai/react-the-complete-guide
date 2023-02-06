69602db83ea0b63b6577fda7a36b307bbacfc8f3
Andrei <andrei@nerdai.io>
2023-02-05 23:56:07 -0500

    fully implement useContext state management

--- section27/react-ts/src/App.tsx
+++ section27/react-ts/src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react';
-
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodoContextProvider from "./store/todos-context";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (text: string) => {
-    const newTodo = new Todo(text);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    })
-
-  };
-
-  const removeTodoHandler = (id: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== id);
-    })
-  };
+  
 
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler}/>
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodoContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodoContextProvider>
   );
 }
 
--- section27/react-ts/src/components/NewTodo.tsx
+++ section27/react-ts/src/components/NewTodo.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useRef } from "react";
-import classes from './NewTodo.module.css';
+import { TodosContext } from "../store/todos-context";
+import classes from "./NewTodo.module.css";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+  const todoCtx = useContext(TodosContext);
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -15,7 +17,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todoCtx.addTodo(enteredText);
   };
 
   return (
